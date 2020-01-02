96484d63531d8ffffa4b501dcd341698b3ced4a2
phatdroid94 <phatdroid94@gmail.com>
2020-01-02 20:23:11 +0700

    finish

--- app/src/main/java/com/example/firebasedatabase08102019/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase08102019/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.firebasedatabase08102019;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -68,5 +74,58 @@ public class MainActivity extends AppCompatActivity {
 //                    }
 //                }
 //            });
+
+        //4 : Gui dang randomid
+//        Sinhvien phat = new Sinhvien("Pham tan phat","26");
+//        Sinhvien khoa = new Sinhvien("Pham truong dang khoa","30");
+//        Sinhvien quan = new Sinhvien("Nguyen hoang quan","28");
+//
+//        myRef.child("giangvien").push().setValue(quan);
+
+
+
+        // Doc ve
+//        myRef.child("sinhvien").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                Sinhvien sinhvien = dataSnapshot.getValue(Sinhvien.class);
+//                Log.d("BBB",sinhvien.getTen());
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
+        // doc ve
+        myRef.child("giangvien").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Sinhvien sinhvien = dataSnapshot.getValue(Sinhvien.class);
+                Log.d("BBB",dataSnapshot.getKey());
+                Log.d("BBB",sinhvien.getTen());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
